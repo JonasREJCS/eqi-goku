203d11c750216b4e98deee855f6e38f7665e4837
Hevelyn Hespanhol <chefinha@localhost.localdomain>
2019-09-06 18:44:04 -0300

    Implementado os arquivos de classes e schemas: fundo, resultado e rentabilidade.

---
+++ src/classes/resultado.class.ts
@@ -0,0 +1,15 @@
+export class Resultado {
+  resultadoId: number
+  planoPrevidencia: string
+  regimeTributarioIR: string
+
+  constructor ({
+    resultadoId,
+    planoPrevidencia,
+    regimeTributarioIR
+  }: Resultado ){
+    this.resultadoId = resultadoId
+    this.planoPrevidencia = planoPrevidencia
+    this.regimeTributarioIR = regimeTributarioIR
+  }
+}
