aa04a26ab973bf25eb7a0c90372c453cf8d0404e
HeveHespanhol <hevelyn.hespanhol@hotmail.com>
2019-09-12 15:21:43 -0300

    Arrumando o arquivo simulacao.route

--- src/routes/simulacao.route.ts
+++ src/routes/simulacao.route.ts
@@ -3,7 +3,9 @@ import express from 'express'
 const simulacaoRouter = express.Router()
 
 import { salvar } from '../controllers/simulacao.controller'
+import { validarSimulacao } from '../middlewares/validacoes/simulacao.middleware'
+import { validarDadosSimulacao } from '../middlewares/validacoes/dados-simulacao.middleware'
 
-simulacaoRouter.post('/v1/gerar-simulacao', salvar)
+simulacaoRouter.post('/v1/salvar-simulacao', validarSimulacao, validarDadosSimulacao, salvar)
 
 export default simulacaoRouter
