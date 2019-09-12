b237d11f949fa0c8b28ed7d6aa5e62eadbbb5623
HeveHespanhol <hevelyn.hespanhol@hotmail.com>
2019-09-12 15:02:43 -0300

    Alteraçao no route

--- src/routes/simulacao.route.ts
+++ src/routes/simulacao.route.ts
@@ -4,7 +4,8 @@ const simulacaoRouter = express.Router()
 
 import { salvarDadosSimulacao } from '../controllers/simulacao.controller'
 import { validarDadosSimulacao } from '../middlewares/validacoes/dados-simulacao.middleware'
+import { validarSimulacao } from '../middlewares/validacoes/simulacao.middleware'
 
-simulacaoRouter.post('/v1/salvar-simulacao', validarDadosSimulacao, salvarDadosSimulacao)
+simulacaoRouter.post('/v1/salvar-simulacao', validarSimulacao, validarDadosSimulacao, salvarDadosSimulacao)
 
 export default simulacaoRouter
